891c98db5df80c8b8a94f4672bb6b0c6f97f5097
Mai01-1 <113573813+Mai01-1@users.noreply.github.com>
2023-07-17 14:04:15 -0300

    Update img.js

--- img.js
+++ img.js
@@ -1,20 +1,3 @@
-function readMore(){
-    let d0 = document.getElementById("dot0");
-    let more0 = document.getElementById("p0");
-    let btn = document.getElementById("btn0");
-
-    if(d0.style.display == "none"){
-        d0.style.display = "inline";
-        btn.innerHTML = "Read more >"; 
-        more0.style.display = "none";
-    } 
-    else{
-        d0.style.display = "none";
-        btn.innerHTML = "< Read less"; 
-        more0.style.display = "inline";
-    }
-}
-
 let scr = document.getElementById("scroll");
 window.onscroll = function(){
     scrollFnc();
@@ -31,14 +14,3 @@ function scrollFnc(){
 function toTop(){
     document.documentElement.scrollTop = 0;
 }
-
-/*function more(){
-    let ext = document.getElementsByClassName(extensions);
-    let adding = document.getElementsByClassName(add);
-    if(adding.style.display == "none"){
-        adding.style.display = "inline";
-    }
-    else{
-        adding.style.display = "none";
-    }
-}*/
