bf59442c5b19b106d3bcb0e39307356cac18449b
Mai <113573813+Mai01-1@users.noreply.github.com>
2023-08-02 11:44:21 -0300

    Add files via upload

---
+++ parallax.js
@@ -0,0 +1,15 @@
+const parallaxSections = document.querySelectorAll('.parallax-section');
+
+parallaxSections.forEach(section => {
+    const speed = parseFloat(section.getAttribute('data-speed'));
+    const position = section.getAttribute('data-position');
+    const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section,
+          start: "top center",
+          scrub: true
+        }
+    });
+
+    tl.to(section, { backgroundPosition: `50% ${position}`, ease: "none" });
+});
